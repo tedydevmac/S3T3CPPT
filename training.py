901628490dbca697063c88496d5f2e3b5d5fba7c
Arth Aggarwal <arth_aggarwal@s2022.ssts.edu.sg>
2024-08-15 10:15:11 +0800

    started the training

---
+++ training.py
@@ -0,0 +1,13 @@
+# Dependencies
+import pandas as pd
+
+# csv reading
+dataset = pd.read_csv('finalPreprocessedDataset.csv')
+
+'''
+To Do:
+1. Load x and y 
+2. define train test split
+3. Linear regression 
+4. Evaluate model
+'''
