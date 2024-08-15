f6c3b12df4c6353c6ac7f9f65ff9e64b38d09d5e
Arth Aggarwal <arth_aggarwal@s2022.ssts.edu.sg>
2024-08-15 10:39:24 +0800

    Added SciKlt learn

--- training.py
+++ training.py
@@ -1,13 +1,27 @@
 # Dependencies
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
-# csv reading
-dataset = pd.read_csv('finalPreprocessedDataset.csv')
 
-'''
+"""
 To Do:
 1. Load x and y 
 2. define train test split
 3. Linear regression 
 4. Evaluate model
-'''
+"""
+
+# csv reading
+dataset = pd.read_csv("finalPreprocessedDataset.csv", index_col=0)
+
+# features
+target_columns = [
+    "orientation=no_orientation",
+    "orientation=asexual",
+    "orientation=heterosexual",
+    "orientation=homosexual",
+    "orientation=bisexual",
+]
+
+x = dataset.drop(target_columns, axis=1)
+y = dataset[target_columns]
