89131823f1793d32a32c934254be84332ed51e4e
Seaa0 <jaron_ng_yu_xuan@s2022.ssts.edu.sg>
2024-08-26 10:59:03 +0800

    added time tracking and infinitely inputting comments to predict

--- training.py
+++ training.py
@@ -14,6 +14,10 @@ from sklearn.model_selection import GridSearchCV
 # import string tokenization function from data manipulation file
 from stringTokenizationFunc import stringTokenize
 
+# for checking the amount of time taken to train
+import time
+startTime = time.time()
+
 """
 To Do:
 1. Fine tune the model by choosing between the models
@@ -55,6 +59,12 @@ cross_val_score = cross_val_score(classifier, X_tfidf, y, cv=5, scoring="accurac
 print("CVal:", cross_val_score)
 print("Mean CVal score:", cross_val_score.mean())
 
+# check how much time it takes to train
+if len(str(int((time.time()-startTime)%60))) < 2:
+    print('Time taken: '+str(int((time.time()-startTime)//60))+':'+'0'+str(int((time.time()-startTime)%60)))
+else:
+    print('Time taken: '+str(int((time.time()-startTime)//60))+':'+str(int((time.time()-startTime)%60)))
+
 # Evaluation by grid search
 param = {
     "estimator__C": [0.1, 1, 10, 100],
@@ -81,28 +91,35 @@ print(
     "Classification Report:\n", classification_report(y_test, y_pred, zero_division=0)
 )
 
-new_comment = "I hate faggots"
-# new_comment = input("Enter the comment: ")
-# string Tokenization, remove punctuation, stopwords, emojis, doing word stemming and replacing common text abbreviations
-new_comment_toke = stringTokenize(new_comment)
-new_comment_not_toke = ""
-# merging tokenised string into one sentence for vectorizer.transform to work
-for word in new_comment_toke:
-    new_comment_not_toke += word + " "
-comment_toke = vectorizer.transform([new_comment_not_toke[:-1]])
-predictions = classifier.predict(comment_toke)
+# check how much time it takes to train
+if len(str(int((time.time()-startTime)%60))) < 2:
+    print('Time taken: '+str(int((time.time()-startTime)//60))+':'+'0'+str(int((time.time()-startTime)%60)))
+else:
+    print('Time taken: '+str(int((time.time()-startTime)//60))+':'+str(int((time.time()-startTime)%60)))
 
-# return values
-print(predictions)
-if predictions[0][0] == 1:
-    print("The message is not offensive to those of the LGBTQ community")
-if predictions[0][1] == 1:
-    print("The message is offensive to heterosexuals")
-if predictions[0][2] == 1:
-    print(
-        "The message is offensive to those of the LGBTQ community especially to the homosexuals"
-    )
-if predictions[0][3] == 1:
-    print(
-        "The message is offensive to those of the LGBTQ community especially the bisexuals"
-    )
+new_comment = "Straight ahh"
+while True:
+    new_comment = input("Enter the comment: ")
+    # string Tokenization, remove punctuation, stopwords, emojis, doing word stemming and replacing common text abbreviations
+    new_comment_toke = stringTokenize(new_comment)
+    new_comment_not_toke = ""
+    # merging tokenised string into one sentence for vectorizer.transform to work
+    for word in new_comment_toke:
+        new_comment_not_toke += word + " "
+    comment_toke = vectorizer.transform([new_comment_not_toke[:-1]])
+    predictions = classifier.predict(comment_toke)
+
+    # return values
+    print(predictions)
+    if predictions[0][0] == 1:
+        print("The message is not offensive to those of the LGBTQ community")
+    if predictions[0][1] == 1:
+        print("The message is offensive to heterosexuals")
+    if predictions[0][2] == 1:
+        print(
+            "The message is offensive to those of the LGBTQ community especially to the homosexuals"
+        )
+    if predictions[0][3] == 1:
+        print(
+            "The message is offensive to those of the LGBTQ community especially the bisexuals"
+        )
